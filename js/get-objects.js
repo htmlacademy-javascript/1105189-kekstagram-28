320234c0bffda856b2f20e3f3b29f3c2319ad969
Максим Бервинов <crash-em@mail.ru>
2023-03-02 15:52:47 +0500

    Разделение на модули.

---
+++ js/get-objects.js
@@ -0,0 +1,33 @@
+import {getRandomInteger} from './utils';
+import {DESCRIPTION, MESSAGES, NAMES} from './data';
+
+let id = 1;
+let commentId = 1;
+
+const getRandomComment = () => {
+  const result = {
+    id: commentId,
+    avatar: `img/avatar-${commentId}.svg`,
+    message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
+    name: NAMES[getRandomInteger(0, NAMES.length - 1)]
+  };
+  commentId += 1;
+  return result;
+};
+
+
+const getArray = () => {
+  const result = {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: DESCRIPTION[getRandomInteger(0, DESCRIPTION.length - 1)],
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({length: getRandomInteger(1, 3)}, getRandomComment)
+  };
+  id += 1;
+  return result;
+};
+
+const getObjects = Array.from({length: 3}, getArray);
+
+export {getObjects};
