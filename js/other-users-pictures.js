9b6ed70ecb7923a551a4f34b0c358af85ad4fdc0
Максим Бервинов <crash-em@mail.ru>
2023-04-19 15:41:41 +0500

    Задача 12.12

--- js/other-users-pictures.js
+++ js/other-users-pictures.js
@@ -16,3 +16,51 @@ const generateOtherUsersPhoto = (array) => {
 };
 
 getData().then((data) => generateOtherUsersPhoto(data));
+
+const defaultFilter = document.querySelector('#filter-default');
+const randomFilter = document.querySelector('#filter-random');
+const discussedFilter = document.querySelector('#filter-discussed');
+
+defaultFilter.addEventListener('click', () => {
+  const dataPhoto = [];
+  getData().then((data) => {
+    dataPhoto.push(...data);
+  }).then(() => {
+    container.querySelectorAll('a.picture').forEach((el) => el.remove());
+    generateOtherUsersPhoto(dataPhoto);
+  });
+});
+
+randomFilter.addEventListener('click', () => {
+  const dataPhoto = [];
+  let index = 0;
+  const getRandomInteger = (min, max) => {
+    const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+    const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+    const result = Math.random() * (upper - lower + 1) + lower;
+    return Math.floor(result);
+  };
+  getData().then((data) => {
+    for (let i = 0; i < 10; i++) {
+      index = getRandomInteger(0, data.length - 1);
+      dataPhoto.push(data[index]);
+      delete data.splice(index, 1);
+    }
+  }).then(() => {
+    container.querySelectorAll('a.picture').forEach((el) => el.remove());
+    let timeoutId;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => generateOtherUsersPhoto(dataPhoto), 2000);
+  });
+});
+
+discussedFilter.addEventListener('click', () => {
+  const dataPhoto = [];
+  getData().then((data) => {
+    dataPhoto.push(...data);
+    dataPhoto.sort((a, b) => b.likes - a.likes);
+  }).then(() => {
+    container.querySelectorAll('a.picture').forEach((el) => el.remove());
+    generateOtherUsersPhoto(dataPhoto);
+  });
+});
