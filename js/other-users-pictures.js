7695796c79bf965f37cd35df5a9a1abcd3339765
Максим Бервинов <crash-em@mail.ru>
2023-04-20 12:23:22 +0500

    Задача 12.12 Доделал задачи по ТЗ. Плюс сделал так, что активный фильтр выделяется.
    Не получилось использовать функцию debounce из теории. Пришлось самому вписывать логику в функцию слушателя. Так и не понял почему не работало через debounce.

--- js/other-users-pictures.js
+++ js/other-users-pictures.js
@@ -1,8 +1,10 @@
 import {getData} from './api.js';
+import {getRandomInteger} from './utils.js';
 
 const container = document.querySelector('.pictures.container');
 const template = document.querySelector('#picture').content;
 const otherUsersPhotoFragment = document.createDocumentFragment();
+let timerIndex;
 
 const generateOtherUsersPhoto = (array) => {
   array.forEach(({url, comments, likes}) => {
@@ -23,44 +25,60 @@ const discussedFilter = document.querySelector('#filter-discussed');
 
 defaultFilter.addEventListener('click', () => {
   const dataPhoto = [];
-  getData().then((data) => {
-    dataPhoto.push(...data);
-  }).then(() => {
-    container.querySelectorAll('a.picture').forEach((el) => el.remove());
-    generateOtherUsersPhoto(dataPhoto);
-  });
+  clearTimeout(timerIndex);
+
+  timerIndex = setTimeout(() => {
+    getData().then((data) => {
+      dataPhoto.push(...data);
+    }).then(() => {
+      container.querySelectorAll('a.picture').forEach((el) => el.remove());
+      generateOtherUsersPhoto(dataPhoto);
+    });
+  }, 500);
+
+  defaultFilter.classList.add('img-filters__button--active');
+  randomFilter.classList.remove('img-filters__button--active');
+  discussedFilter.classList.remove('img-filters__button--active');
 });
 
 randomFilter.addEventListener('click', () => {
   const dataPhoto = [];
   let index = 0;
-  const getRandomInteger = (min, max) => {
-    const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-    const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-    const result = Math.random() * (upper - lower + 1) + lower;
-    return Math.floor(result);
-  };
-  getData().then((data) => {
-    for (let i = 0; i < 10; i++) {
-      index = getRandomInteger(0, data.length - 1);
-      dataPhoto.push(data[index]);
-      delete data.splice(index, 1);
-    }
-  }).then(() => {
-    container.querySelectorAll('a.picture').forEach((el) => el.remove());
-    let timeoutId;
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => generateOtherUsersPhoto(dataPhoto), 2000);
-  });
+  clearTimeout(timerIndex);
+
+  timerIndex = setTimeout(() => {
+    getData().then((data) => {
+      for (let i = 0; i < 10; i++) {
+        index = getRandomInteger(0, data.length - 1);
+        dataPhoto.push(data[index]);
+        delete data.splice(index, 1);
+      }
+    }).then(() => {
+      container.querySelectorAll('a.picture').forEach((el) => el.remove());
+      generateOtherUsersPhoto(dataPhoto);
+    });
+  }, 500);
+
+  defaultFilter.classList.remove('img-filters__button--active');
+  randomFilter.classList.add('img-filters__button--active');
+  discussedFilter.classList.remove('img-filters__button--active');
 });
 
 discussedFilter.addEventListener('click', () => {
   const dataPhoto = [];
-  getData().then((data) => {
-    dataPhoto.push(...data);
-    dataPhoto.sort((a, b) => b.likes - a.likes);
-  }).then(() => {
-    container.querySelectorAll('a.picture').forEach((el) => el.remove());
-    generateOtherUsersPhoto(dataPhoto);
-  });
+  clearTimeout(timerIndex);
+
+  timerIndex = setTimeout(() => {
+    getData().then((data) => {
+      dataPhoto.push(...data);
+      dataPhoto.sort((a, b) => b.likes - a.likes);
+    }).then(() => {
+      container.querySelectorAll('a.picture').forEach((el) => el.remove());
+      generateOtherUsersPhoto(dataPhoto);
+    });
+  }, 500);
+
+  defaultFilter.classList.remove('img-filters__button--active');
+  randomFilter.classList.remove('img-filters__button--active');
+  discussedFilter.classList.add('img-filters__button--active');
 });
